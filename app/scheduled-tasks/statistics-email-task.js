4609b574ddab465c99a4de81382e64e3f3a4fa0f
mmaquiel <m.maquiel1991@gmail.com>
2015-07-15 17:09:30 -0300

    Add job to send an email with statistics

---
+++ app/scheduled-tasks/statistics-email-task.js
@@ -0,0 +1,33 @@
+var CronJob = require('cron').CronJob;
+var mailer = require('../mailer/mailer');
+var moment = require('moment');
+var Visitor = require('../models/visitor');
+
+exports.schedule = function () {
+  var job = new CronJob("00 00 00 * * *", function() {
+    console.log("Statistics email task started");
+    var today = moment().startOf('day');
+    var tomorrow = moment(today).add(1, 'days');
+
+    var lastDayCountQuery = Visitor.where({
+      createdAt: {
+        $gte: today.toDate(),
+        $lt: tomorrow.toDate()
+      }
+    }).count();
+
+    Visitor.count(function (err, totalCount) {
+      lastDayCountQuery.exec(function (err, lastDayCount) {
+        mailer.sendStatistics({
+          totalVisits: totalCount,
+          lastDayVisits: lastDayCount
+        });
+
+      });
+    });
+
+  });
+
+  job.start();
+  console.log("Statistics email scheduled");
+}

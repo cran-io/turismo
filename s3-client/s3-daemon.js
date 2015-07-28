eee84cb9eaf2452b532598bc3a0f6075a13d6584
mmaquiel <m.maquiel1991@gmail.com>
2015-07-28 19:08:17 -0300

    Revert to 3356c1d45fc6aa0b3e1978131f181fa963a66c0e

--- s3-client/s3-daemon.js
+++ s3-client/s3-daemon.js
@@ -5,23 +5,18 @@ var AWS = require('aws-sdk');
 var mime = require('mime');
 var gm = require('gm').subClass({ imageMagick: true });
 var walk = require('walk');
-var Q = require("q");
-var readFile = Q.nfbind(fs.readFile);
 
 AWS.config.region = "sa-east-1";
 
-var s3 = new AWS.S3({params: {Bucket: config.S3.bucket} });
-var uploadToS3 = Q.nbind(s3.upload);
+var s3 = new AWS.S3({params: {Bucket: "turismo-site"} });
 var walker = walk.walk(config.photos_dir, { followLinks: false });
 
 var separator = process.platform === "win32" ? "\\" : "/";
 
 walker.on("file", function (root, fileStat, next) {
   var path = [root, fileStat.name].join(separator);
-  uploadPhotos(path)
-    .then(function () {
-      next();
-    });
+  uploadPhotos(path);
+  next();
 });
 
 watch(config.photos_dir, function (path) {
@@ -37,68 +32,44 @@ function uploadPhotos(path) {
     return;
   }
 
+
   var pathArray = path.split(separator);
   var fileName = pathArray[pathArray.length-1];
   var sourceFileName = "sources/" + fileName;
 
-  var deferred = Q.defer();
 
-  readFile(path)
-    .then(function (file) {
-      var photoMimeType = mime.lookup(path);
-      var params = {
-        Key: sourceFileName,
-        Body: file,
-        ContentType: photoMimeType,
-        ACL: 'public-read'
-      };
+  fs.readFile(path, function (err, file) {
+    if(err) throw err;
 
-      uploadToS3(params)
-        .then(function (data) {
-          console.log("Uploaded: ", data.Location);
-          return resize(file);
-        })
-        .then(function (thumbnail) {
-          var params = {
-            Key: "thumbnails/" + fileName,
-            Body: thumbnail,
-            ContentType: photoMimeType,
-            ACL: 'public-read'
-          };
-          return uploadToS3(params);
-        })
-        .then(function (data) {
-          console.log("Uploaded: ", data.Location);
-          deferred.resolve();
-        });
-      })
-      .catch(function (e) {
-        deferred.reject(e);
-      });
+    var photoMimeType = mime.lookup(path);
+    var params = {
+      Key: sourceFileName,
+      Body: file,
+      ContentType: photoMimeType
+    };
 
-  return deferred.promise;
-}
+    s3.upload(params, function (err, data) {
+      if(err) throw err;
 
-function uploadToS3(params) {
-  var deferred = Q.defer();
-
-  s3.upload(params, function (err, data) {
-      if(err) deferred.reject(err);
-      deferred.resolve(data);
-  });
-
-  return deferred.promise;
-}
-
-function resize(file) {
-  var deferred = Q.defer();
-
-  gm(file)
-    .resize(639, 392)
-    .toBuffer(function (err, data) {
-        if(err) deferred.reject();
-        deferred.resolve(data);
+      console.log("Uploaded: ", data.Location);
     });
 
-  return deferred.promise;
+    gm(file)
+      .resize(639, 392)
+      .toBuffer(function(err, data) {
+        if(!err){
+          var params = {
+            Key: "thumbnails/" + fileName,
+            Body: data,
+            ContentType: photoMimeType
+          };
+
+          s3.upload(params, function (err, data) {
+            if(err) throw err;
+
+            console.log("Uploaded: ", data.Location);
+          });
+        }
+      });
+  });
 }

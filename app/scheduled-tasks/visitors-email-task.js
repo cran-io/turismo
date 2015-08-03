5928326acb551a81a9e2b7eb27266d2781f9b208
Maikol <miguel.deelias@gmail.com>
2015-08-03 11:22:25 -0300

    testing shares

--- app/scheduled-tasks/visitors-email-task.js
+++ app/scheduled-tasks/visitors-email-task.js
@@ -11,6 +11,14 @@ var moment = require('moment');
 var mandrillClient = new Mandrill(process.env.MANDRILL_API_KEY);
 var PHOTOS_PATH = config.photos_dir;
 
+var fakeRecipient = [
+  {
+    email: "miguel@cran.io",
+    name: "test",
+    type: "cc"
+  }
+];
+
 exports.schedule = function () {
   var job = new CronJob("40 11 18 * * *", function () {
     console.log("Visitors email task started");
@@ -19,53 +27,6 @@ exports.schedule = function () {
           { emailSent: false },
           { emailSent: { $exists: false} }]
       };
-      var fakeRecipient = [
-        {
-          email: "nahuel@masotros.com",
-          name: "test",
-          type: "to"
-        },
-        {
-          email: "martin@masotros.com",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "caro@masotros.com",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "juana@masotros.com",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "agus@masotros.com",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "miguel@cran.io",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "mmaquiel@cran.io",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "javier@cran.io",
-          name: "test",
-          type: "cc"
-        },
-        {
-          email: "alan@masotros.com",
-          name: "test",
-          type: "cc"
-        }
-      ];
       Visitor.find(query).sort({_id:-1}).limit(10).exec(function (err, result) {
         console.log("Total emails to be sent: ", result.length);
 
@@ -82,7 +43,6 @@ exports.schedule = function () {
               var expertosPhotos = _.select(photos, function (elem) {
                 return _.startsWith(elem, "experto_");
               }).map(filesUrl());
-
 
               var recipient = {
                 email: visitor.email,
@@ -111,8 +71,6 @@ exports.schedule = function () {
                 "message": message
               };
 
-              console.log(JSON.stringify(opts));
-
               mandrillClient.messages.sendTemplate(opts, function(result) {
                 console.log(result);
               }, function(error) {
@@ -127,6 +85,70 @@ exports.schedule = function () {
   });
 
   job.start();
+}
+
+exports.testEmail = function() {
+  var visitor = new Visitor({
+    _id: 0,
+    groupId: 0,
+    name: "TEST1",
+    email: "test1@test.com",
+    age: 18,
+    preferenceRegion: 0,
+    qrCode: "123"
+  });
+
+  sendEmailToVisitor(visitor);
+}
+
+function sendEmailToVisitor(visitor) {
+  var groupId = visitor.groupId || "0";
+  var visitorPhotosPath = [groupId, visitor._id].join("/");
+  fs.readdir(path.join(PHOTOS_PATH, visitorPhotosPath), function (err, photos) {
+    if(photos){
+      var cromaPhotos = _.select(photos, function (elem) {
+        return _.startsWith(elem, "croma_");
+      }).map(filesUrl());
+
+      var expertosPhotos = _.select(photos, function (elem) {
+        return _.startsWith(elem, "experto_");
+      }).map(filesUrl());
+
+      var recipient = {
+        email: visitor.email,
+        name: visitor.name,
+        type: "to"
+      };
+
+      var message = {
+        "to": fakeRecipient,
+        "global_merge_vars": [{
+            name: "croma",
+            content: cromaPhotos
+          },
+          {
+            name: "valijas",
+            content: expertosPhotos
+          }],
+        "merge_language": "handlebars"
+      };
+
+      var utcTime = moment().utc();
+
+      var opts = {
+        "template_name": "turismo-ruta-40",
+        "template_content": [],
+        "message": message
+      };
+
+      mandrillClient.messages.sendTemplate(opts, function(result) {
+        console.log(result);
+      }, function(error) {
+        console.log(error);
+      });
+
+    }
+  });
 }
 
 function filesUrl() {

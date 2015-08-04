5a6ed0a4ba6177b663d5494c75930db5e905f8fe
Maikol <miguel.deelias@gmail.com>
2015-08-04 19:46:59 -0300

    sending to correct recipient

--- app/scheduled-tasks/visitors-email-task.js
+++ app/scheduled-tasks/visitors-email-task.js
@@ -90,7 +90,7 @@ function sendEmailToVisitor(visitor) {
       };
 
       var message = {
-        "to": fakeRecipient,
+        "to": recipient,
         "global_merge_vars": [{
             name: "croma",
             content: cromaPhotos
